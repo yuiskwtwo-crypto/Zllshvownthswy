fa157ca3dbd82f7e8e77a2ad64f7f5a68931541b
yoman <yuiskwtwo@gmail.com>
2026-08-31 03:34:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -697,7 +697,7 @@ function answerQuiz1(isYes) {
     } else {
         playSound('error');
         feedback.innerText = "Yaaahh harusnyaa jangan dibaca dulu..Yaudah pencet dah ini..";
-        noBtn.innerText = "YES SETUJU JUGA 😁";
+        noBtn.innerText = "YAUDAHLAH..";
         noBtn.onclick = () => answerQuiz1(true);
     }
 }
